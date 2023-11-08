02bd8458bce9d265a513436b3ff2e93b0d24d253
Siya <sbaloyi963@gmail.com>
2023-11-08 05:18:55 +0200

    google keep

--- app.js
+++ app.js
@@ -28,10 +28,67 @@ class App {
     this.$sidebar = document.querySelector(".sidebar");
     this.$sidebarActiveItem = document.querySelector(".active-item");
 
+    this.$app = document.querySelector("#app");
+    this.$firebaseAuthContainer = document.querySelector("#firebaseui-auth-container");
+    this.$authUserText = document.querySelector(".auth-user");
+    this.$logoutButton = document.querySelector(".logout");
+
+
+    this.ui = new firebaseui.auth.AuthUI(auth);
+    this.handleAuth();
+
 
     this.addEventListeners();
     this.displayNotes();
   }
+
+  handleAuth() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            console.log(user.uid);
+            this.userId = user.uid;
+            this.$authUserText.innerHTML = user.displayName;
+            this.redirectToApp();
+        } else {
+            this.redirectToAuth();
+        }
+    });
+}
+
+handleLogout() {
+  firebase.auth().signOut().then(() => {
+      this.redirectToAuth();
+  }).catch((error) => {
+      console.log("ERROR OCCURED", error);
+  });
+}
+
+redirectToApp() {
+  this.$firebaseAuthContainer.style.display = "none";
+  this.$app.style.display = "block";
+  this.fetchNotesFromDB();
+}
+
+redirectToAuth() {
+  this.$firebaseAuthContainer.style.display = "block";
+  this.$app.style.display = "none";
+
+  this.ui.start('#firebaseui-auth-container', {
+      callbacks: {
+          signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+    
+              console.log("authResult", authResult.user.uid)
+              this.userId = authResult.user.uid;
+              this.$authUserText.innerHTML = user.displayName;
+              this.redirectToApp();
+          }
+      },
+      signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+  });
+}
 
   addEventListeners() {
     document.body.addEventListener("click", (event) => {
